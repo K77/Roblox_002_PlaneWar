fd82139a33cfb5a670fbf5e199fce96b82691f7c
K_XPS15 <jizj@icloud.com>
2023-04-25 21:55:42 +0800

    每个人头顶显示名字

--- src/3_server/init_server.server.lua
+++ src/3_server/init_server.server.lua
@@ -4,25 +4,7 @@ _G.ConstShare = require(_G.PathRojoShare.const_share)
 local Players = game:GetService("Players")
 local s_prompt = require(script.Parent.s_prompt)
 local s_status = require(script.Parent.s_status)
+local s_head_name = require(script.Parent.s_head_name)
 
 print("server inited!!!")
 
-Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(char)
-        -- local camera = workspace.CurrentCamera :: Camera
-        --camera.CFrame = CFrame.new(-1590.21, 105.082, 90.189,-36.945, 3.501, 0)
-        -- task.wait(1)
-        -- camera.CFrame = CFrame.new(-1590.21, 105.082, 90.189) * CFrame.fromEulerAnglesXYZ(math.rad(-36.945), 0, 0)
-        print("client inited!CharacterAdded!!")
-        
-        --local char = script.Parent
-        -- local player = game.Players:GetPlayerFromCharacter(char)
-        local humanoid = char:WaitForChild("Humanoid")
-        humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
-        local head = char:WaitForChild("HumanoidRootPart")
-        local headName = game.ServerStorage.HeadName:Clone()
-        headName.Parent = head
-        headName.Adornee = head
-        headName.TextLabel.Text = player.Name
-    end)
-end)
